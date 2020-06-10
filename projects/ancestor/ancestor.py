19e89c11ea7491b188356c645cf6b62c84f1ba5b
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-06-10 17:17:18 -0500

    ancestors passing

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,4 @@
+ #make graph to store the relations
 class Graph:
     def __init__(self):
         self.verts = {}
@@ -13,21 +14,37 @@ class Graph:
         return self.verts[vert_id]
     
 def earliest_ancestor(ancestors, starting_node):
-    pass
-    
-    
-#make graph to store the relations
-#add nodes to the graph = for each input make a 'node'
-#connect the nodes with edges to make the relation complete
-#use bfs to find the earliest ancestor
-    #instead of returning the path like yesterday just return the node(index, value,etc)
-
-if __name__ == '__main__':
-            #skip                   #skip   #skip   #4{5}    #4{5,8} #skip
-    test = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+    #check for longest path to return but keep track of the other paths
     graph = Graph()
-    for pair in test:
-        graph.add_vert(pair[0])
+    for pair in ancestors:
         graph.add_vert(pair[1])
-        graph.add_edge(pair[0], pair[1])
+    for pair2 in ancestors:
+        graph.add_vert(pair2[0])
+    for pair in ancestors:
+        graph.add_edge(pair[1], pair[0])
+    
     print(graph.verts)
+    if len(graph.verts[starting_node]) == 0:
+        return -1
+    
+    stack = []
+    stack.append([starting_node])
+    current_path = stack
+    while len(stack) > 0:
+        old_path = current_path
+        current_path = stack.pop()
+        current = current_path[-1]
+        
+        for next_node in graph.get_neighbor(current):
+            new_path = list(current_path)
+            new_path.append(next_node)
+            stack.append(new_path)
+            
+    if len(current_path) > len(old_path):
+        return current_path[-1]
+    elif len(current_path) == len(old_path):
+        if old_path[-1] < current_path[-1]:
+            return old_path[-1]
+if __name__ == '__main__':
+    test = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+    print(earliest_ancestor(test, 9))
