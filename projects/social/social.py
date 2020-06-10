dfda154b84b13251bfaf9f42724546cfee3518f2
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-06-10 14:43:59 -0500

    generating friendships works

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,4 @@
+import random
 class User:
     def __init__(self, name):
         self.name = name
@@ -42,12 +43,25 @@ class SocialGraph:
         self.last_id = 0
         self.users = {}
         self.friendships = {}
-        # !!!! IMPLEMENT ME
 
         # Add users
-
+        for i in range(0, num_users):
+            self.add_user(i)
         # Create friendships
-
+        possible_friendships = []
+        #avoiding dupes by checking the first number is smaller than the 2nd
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+        
+        #shuffle the possible friendships
+        random.shuffle(possible_friendships)
+        
+        #create friendships
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
+    
     def get_all_social_paths(self, user_id):
         """
         Takes a user's user_id as an argument
